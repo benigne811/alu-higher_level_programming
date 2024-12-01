4dc1439e6dcab87960eaad9de8aa96adf98d09e9
root <b.uwitonze@alustudent.com>
2024-12-01 20:33:01 +0000

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
+# python script that fetches 'https://alu-intranet.hbtn.io/status'
+""" fetch 'https://intranet.hbtn.io/status'"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
